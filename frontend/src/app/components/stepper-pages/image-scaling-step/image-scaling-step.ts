1dddfeb6386aa0270bce3167b302541f33a83f8f
Tmbaldwi <Tyler.Baldwin.Work@gmail.com>
2026-03-17 22:23:52 -0700

    added retry ability for service calls, as well as proper destruction

--- frontend/src/app/components/stepper-pages/image-scaling-step/image-scaling-step.ts
+++ frontend/src/app/components/stepper-pages/image-scaling-step/image-scaling-step.ts
@@ -4,11 +4,12 @@ import { ImageService } from '../../../services/image-service';
 import { MatButtonModule } from '@angular/material/button';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatIconModule } from '@angular/material/icon';
-import { HttpErrorResponse } from '@angular/common/http';
 import { FormGroup } from '@angular/forms';
 import { ImageRescaleResponse } from '../../../services/models/image-rescale-response.model';
 import { ImageFrame } from "../../common/image-frame/image-frame";
 import { clearCanvas, displayBitmapOnCanvas } from '../../../utility/canvas.utils';
+import { catchError, EMPTY, Subject, switchMap, take, tap } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-image-scaling-step',
@@ -24,16 +25,47 @@ export class ImageScalingStep {
 
   private stepper = inject(CdkStepper);
   private service = inject(ImageService);
+  private rescaleImage$ = new Subject<void>();
 
   public imageRescale : ImageRescaleResponse | undefined = undefined;
   public isLoading = signal(false);
   public errorMessage = signal<string | null>(null);
 
+  constructor(){
+    this.rescaleImage$.pipe(
+      takeUntilDestroyed(),
+      tap(() => {
+        this.isLoading.set(true);
+        this.errorMessage.set(null);
+      }),
+      switchMap(() => 
+        this.service.getRescaledImage(this.imageHistoryForm().get('originalImage')?.value).pipe(
+          take(1),
+          catchError(err => {
+            this.errorMessage.set("Something went wrong, please try again");
+            console.error(err);
+
+            this.isLoading.set(false);
+            return EMPTY;
+          })
+        ) 
+      )
+    ).subscribe({
+      next: (res) => {
+        this.imageRescale = res;
+        this.imageHistoryForm().get('scaledImageBitmap')?.setValue(res.scaledImageBitmap);
+        displayBitmapOnCanvas(res.scaledImageBitmap, this.canvasRef.nativeElement);
+
+        this.isLoading.set(false);
+      },
+    })
+  }
+
   ngOnInit(){
     this.stepper.selectionChange
       .subscribe(e => {
         if(this.imageNeedsRescaling(e)){
-          this.onStepBegin();
+          this.rescaleOriginalImage();
         }
       })
   }
@@ -47,49 +79,13 @@ export class ImageScalingStep {
     return this.imageHistoryForm().get('scaledImageBitmap')?.invalid;
   }
 
-  onStepBegin() {
+  rescaleOriginalImage() {
     this.imageRescale = undefined;
     clearCanvas(this.canvasRef.nativeElement);
 
     this.isLoading.set(true);
     this.errorMessage.set(null);
 
-    this.getRescaledImage();
-  }
-
-  getRescaledImage() {
-    const originalImage: File = this.imageHistoryForm().get('originalImage')?.value;
-
-    this.service.getRescaledImage(originalImage).subscribe({
-      next: (res) => {
-        this.imageRescale = res;
-
-        this.imageHistoryForm().get('scaledImageBitmap')?.setValue(res.scaledImageBitmap);
-
-        displayBitmapOnCanvas(res.scaledImageBitmap, this.canvasRef.nativeElement);
-      },
-
-      error: (error: unknown) => {
-        if (error instanceof HttpErrorResponse) {
-          this.errorMessage.set(
-            error.error?.detail ||
-            error.message ||
-            'Server error occurred'
-          );
-        } else if (error instanceof Error) {
-          this.errorMessage.set('Error occurred: ' + error.message);
-        } else {
-          this.errorMessage.set(
-            'An unknown error occurred. Please try again.'
-          );
-        }
-
-        this.isLoading.set(false);
-      },
-
-      complete: () => {
-        this.isLoading.set(false);
-      }
-    });
+    this.rescaleImage$.next();
   }
 }
